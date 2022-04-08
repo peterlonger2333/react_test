089ce134ec08d3086cb9bc6fca86e6acc61b6062
peterlonger2333 <becauseimbatman2333@gmail.com>
2022-04-08 18:36:28 +0800

    null

---
+++ public/assets/js/Maker.js
@@ -0,0 +1,4 @@
+function print(){
+    var a=document.getElementById("maker").value;
+    alert(a);
+}
---
+++ public/assets/js/Taker.js
@@ -0,0 +1,38 @@
+// import React, { Component }  from 'react';
+// import ReactDOM from 'react-dom';
+//
+// class Token extends Component{
+// 	constructor(props){
+// 		super(props);
+// 		this.state = {
+// 			val:''
+// 		}
+// 	}
+//
+// 	handleChange(e){
+// 		this.setState({
+// 			val:e.target.value,
+// 		})
+// 	}
+//
+// 	render(){
+// 		return(
+// 			<div>
+// 				<input type="text" placeholder="token ID in the NFT contract" onChange={this.handleChange.bind(this)} defaultValue={this.state.val}/>
+// 			</div>
+// 		)
+// 	}
+// }
+// const domContainer = document.querySelector('#token');
+// ReactDOM.render(Token, domContainer);
+//
+// // ReactDOM.render(
+// //   <h1>Hello, world!</h1>,
+// //   document.getElementById('token')
+// // );
+
+function print(){
+	var a=document.getElementById("taker").value;
+	alert(a);
+}
+
